0f98b52ea68a0542a3fe7a68b66825aaa4135759
kenny132652 <73508417+kenny132652@users.noreply.github.com>
2024-11-29 15:20:41 +0800

    Add files via upload
    加入品牌列表

--- MallSpringTest/src/main/java/com/example/demo/model/Brand.java
+++ MallSpringTest/src/main/java/com/example/demo/model/Brand.java
@@ -2,14 +2,25 @@ package com.example.demo.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+
+
+@NoArgsConstructor
+@Setter
+@Getter
 @Entity
 @Table(name = "Brand")
 public class Brand {
@@ -18,50 +29,14 @@ public class Brand {
 	private int brandId;
 
 	private String brandName;
-	private String brandCountry;
-	private String brandDescription;
+	private String brandInfo;
+	@Lob
+	private byte[] brandPhoto;
 
-	// 反向映射，指示該 Category 對應多個 Item
+	// 反向映射，指示該 Brand 對應多個 Item
+	@JsonIgnore
 	@OneToMany(mappedBy = "brand", fetch = FetchType.LAZY)
 	private List<Item> items;
 
-	public int getBrandId() {
-		return brandId;
-	}
-
-	public void setBrandId(int brandId) {
-		this.brandId = brandId;
-	}
-
-	public String getBrandName() {
-		return brandName;
-	}
-
-	public void setBrandName(String brandName) {
-		this.brandName = brandName;
-	}
-
-	public String getBrandcountry() {
-		return brandCountry;
-	}
-
-	public void setBrandcountry(String brandCountry) {
-		this.brandCountry = brandCountry;
-	}
-
-	public String getBrandDescription() {
-		return brandDescription;
-	}
-
-	public void setBrandDescription(String brandDescription) {
-		this.brandDescription = brandDescription;
-	}
-
-	public List<Item> getItems() {
-		return items;
-	}
-
-	public void setItems(List<Item> items) {
-		this.items = items;
-	}
 }
+
